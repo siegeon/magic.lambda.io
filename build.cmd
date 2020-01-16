50bbb0013d34ea0fc27dd342eec26d732029a313
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:26:22 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,8 @@
+
+set version=%1
+set key=%2
+
 cd %~dp0
+
 dotnet build magic.lambda.io/magic.lambda.io.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet nuget push magic.lambda.io/bin/Release/magic.lambda.io.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
