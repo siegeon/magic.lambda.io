9a5cc13d47506e3073b9d17dfa9b833d37bfa305
Thomas Hansen <thomas@gaiasoul.com>
2020-05-25 13:16:14 +0300

    Create RootFolder.cs

---
+++ magic.lambda.io/folders/RootFolder.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using System.IO;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+using magic.lambda.io.contracts;
+using magic.lambda.io.utilities;
+
+namespace magic.lambda.io.folders
+{
+    /// <summary>
+    /// [io.folders.root] slot for returning root path of system.
+    /// </summary>
+    [Slot(Name = "io.folders.root")]
+    public class RootFolder : ISlot
+    {
+        readonly IRootResolver _rootResolver;
+
+        /// <summary>
+        /// Constructs a new instance of your type.
+        /// </summary>
+        /// <param name="rootResolver">Instance used to resolve the root folder of your app.</param>
+        public RootFolder(IRootResolver rootResolver)
+        {
+            _rootResolver = rootResolver ?? throw new ArgumentNullException(nameof(rootResolver));
+        }
+
+        /// <summary>
+        /// Implementation of slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = _rootResolver.RootFolder;
+        }
+    }
+}
